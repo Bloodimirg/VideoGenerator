e53b10a7dfb9a00d1c5826db1b84690a069ee207
Bloodimirg <bloodimirg@gmail.com>
2024-09-16 13:35:05 +0300

    добавлена модель видео генератора

--- generator/models.py
+++ generator/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+NULLABLE = {'blank': True, 'null': True}
 
-# Create your models here.
+class Video(models.Model):
+
+    video_file = models.FileField(upload_to='media/video/')
+
+    title = models.CharField(max_length=100, **NULLABLE, verbose_name='Название')
+    description = models.TextField(**NULLABLE, verbose_name='Описание')
+    uploaded_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата загрузки')
+
+    def __str__(self):
+        return self.title or 'Видео'
+
+    class Meta:
+        verbose_name = 'Видео'
+        verbose_name_plural = 'Видео'
